22ed0891e028e2caef946536657b430702d77eee
BabediPaai <babedipaai19@gmail.com>
2022-09-28 01:32:41 +0200

    Created and modified the DevicesRepository class

---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/DevicesRepository.cs
@@ -0,0 +1,24 @@
+﻿using DeviceManagement_WebApp.Data;
+using DeviceManagement_WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public class DevicesRepository : GenericRepository<Device>, IDevicesRepository
+    {
+        public DevicesRepository(ConnectedOfficeContext context) : base(context)
+        {
+        }
+
+        public Device GetMostRecentCategory()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Device GetMostRecentDevice()
+        {
+            return _context.Device.OrderByDescending(device => device.DateCreated).FirstOrDefault();
+        }
+    }
+}
