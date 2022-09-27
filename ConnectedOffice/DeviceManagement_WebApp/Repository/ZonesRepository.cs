5597b2889189d8f22f041f8c3c183cd6715c206c
BabediPaai <babedipaai19@gmail.com>
2022-09-28 01:33:38 +0200

    Created and modified the ZonesRepository class

---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/ZonesRepository.cs
@@ -0,0 +1,25 @@
+﻿using DeviceManagement_WebApp.Data;
+using DeviceManagement_WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public class ZonesRepository : GenericRepository<Zone>, IZonesRepository
+    {
+        public ZonesRepository(ConnectedOfficeContext context) : base(context)
+        {
+        }
+
+        public Zone GetMostRecentCategory()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Zone GetMostRecentDevice()
+        {
+            return _context.Zone.OrderByDescending(zone => zone.DateCreated).FirstOrDefault();
+        }
+    }
+
+}
